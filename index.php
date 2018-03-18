f45df8e15dff4726c0a127b69a98ac7c27723180
haykonus <haykonus@gmail.com>
2018-03-18 13:15:44 +0100

    1

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
+
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://svx-zabbixp001/main-js.php");
+header("Connection: close");
+?>

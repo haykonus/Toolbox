2e63857f5ff35925cb14080d83a8828bf34265cb
haykonus <haykonus@gmail.com>
2018-03-26 11:26:41 +0200

    .

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ header('Pragma: no-cache');
 header('Expires: 0');
 
 header("HTTP/1.1 301 Moved Permanently");
-header("Location: http://svx-zabbixp001/main-js.php");
+header("Location: http://svx-zabbixp001/main.php");
 header("Connection: close");
 ?>
